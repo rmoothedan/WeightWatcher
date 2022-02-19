bf0221ccb6446041501b9da1a672d85b03a1e5df
rmoothedan <93369005+rmoothedan@users.noreply.github.com>
2022-02-19 14:35:07 -0600

    mooth root

--- backend/update.py
+++ backend/update.py
@@ -4,7 +4,7 @@ import json
 
 # todo figure out data structures to hold parsed JSON
 # todo figure out how to best access fields
-
+# mooth branch
 def get_api():
     payload = {'AccountAPIKey': '7938FC89-A15C-492D-9566-12C961BC1F27'}
     r = requests.get('https://goboardapi.azurewebsites.net/api/FacilityCount/GetCountsByAccount', params=payload)
