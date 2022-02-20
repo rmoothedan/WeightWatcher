faadeb8b0852824a1b053bd83c64aa62ff1e51fb
Corey Steinhauser <steinhauser2@wisc.edu>
2022-02-20 12:22:17 -0600

    finished parse user data

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import update
+import schedule
 
 app = FastAPI()
 
@@ -16,3 +17,6 @@ def print_api():
 def print_api():
     return update.get_weeks()
 
+@app.get("/schedule.py/parseUserData")
+def print_api():
+    return schedule.parseUserData("sunday 12:30PM-04:30PM,07:30PM-10:30PM")
--- backend/schedule.py
+++ backend/schedule.py
@@ -1,6 +1,25 @@
 
 
-def parseUserData():
-    userTimes = 
+def parseUserData(timeString):
 
-def getTimes():
+    day = timeString[0].upper() + timeString[1:timeString.index(" ")]
+    timeString = timeString[timeString.index(" ") + 1:]
+    userTimes = timeString.split(',')
+    startTimes = []
+    endTimes = []
+    
+    for i in range(len(userTimes)):
+        start, end = userTimes[i].split("-", 1)
+        startTimes.append(start)
+        endTimes.append(end)
+
+        if startTimes[i][5] == 'P' and startTimes[i][0:2] != "12":
+            startTimes[i] = str(int(startTimes[i][0:2]) + 12) + startTimes[i][2:5]
+
+        if endTimes[i][5] == 'P'and endTimes[i][0:2] != 12:
+            endTimes[i] = str(int(endTimes[i][0:2]) + 12) + endTimes[i][2:5]
+
+        startTimes[i] = startTimes[i][0:5]
+        endTimes[i] = endTimes[i][0:5]
+
+    return day, startTimes, endTimes
